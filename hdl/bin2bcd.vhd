5b77d14709bd8575c49479fe7211ae271234077d
sir-drako <67859416+sir-drako@users.noreply.github.com>
2021-03-21 20:58:28 +0100

    Modify bin2bcd module and its testbench to use digit type package

--- hdl/bin2bcd.vhd
+++ hdl/bin2bcd.vhd
@@ -4,6 +4,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.digit_types.bcd_digit;
 
 entity bin2bcd is
     generic (
@@ -13,12 +14,10 @@ entity bin2bcd is
         NUM_BCD_DIGITS: integer := 4
     );
     port ( 
-        bin_in : in  std_logic_vector ((BIN_IN_LENGTH-1) downto 0);
+        bin_in : in  std_logic_vector((BIN_IN_LENGTH-1) downto 0);
 
-        -- output vector groups all values of the BCD digits
-        -- ex. if NUM_BCD_DIGITS = 4, then bcd_out(3 downto 0) corresponds to
-        -- BCD digit 0 (unity), bcd(7 downto 4) corresponds to BCD digit 1, (tens), etc.
-        bcd_out: out std_logic_vector((NUM_BCD_DIGITS*4-1) downto 0)
+        -- an array of N number of BCD digits each with 4 bits (see type package)
+        bcd_out: out bcd_digit((NUM_BCD_DIGITS-1) downto 0) := (others => (others => '0'))
     );
 end bin2bcd;
 
@@ -28,7 +27,9 @@ begin
     process(bin_in)
         -- this holds the input binary number
         variable temp : std_logic_vector ((BIN_IN_LENGTH-1) downto 0);
-        -- variable to store the output bcd number
+
+        -- each BCD digit takes up 4 bits, so we can easily determine the total bit width
+        -- needed for this variable based on the generic
         variable bcd_temp : unsigned ((NUM_BCD_DIGITS*4-1) downto 0) := (others => '0');
   begin
     -- zero the bcd variable
@@ -37,21 +38,22 @@ begin
     -- read input into temp variable
     temp((BIN_IN_LENGTH-1) downto 0) := bin_in;
         
-    -- **** this could be optimized, we do not need to check and add 3 for the 
-    -- first 3 iterations as the number can never be > 4 ******
-
-    -- loop through each bit for the input binary vector
+    -- following loop could be optimized since we do not need to check and 
+    -- add 3 for the first 3 iterations (number can never be > 4)
     for i in 0 to (BIN_IN_LENGTH-1) loop
 
         -- loop through each BCD digit to check if result of the shifting number is greater than 4
         for j in 0 to (NUM_BCD_DIGITS-1) loop
-            -- algorithm to check each BCD digit LSB to MSB (3 downto 0, 7 downto 4, ... etc.)
+
+            -- algorithm to check each BCD digit going from LSB to MSB 
+            -- (3 downto 0, 7 downto 4, ... etc.)
             if bcd_temp((3+4*j) downto j*4) > 4 then 
                 bcd_temp((3+4*j) downto j*4):= bcd_temp((3+4*j) downto j*4) + 3;
             end if;
+
         end loop;
     
-      -- shift bcd_temp left by 1 bit, copy msb of temp into lsb of bcd_temp
+      -- shift bcd_temp left by 1 bit, copy MSB of temp into LSB of bcd_temp
       bcd_temp := bcd_temp(NUM_BCD_DIGITS*4-2 downto 0) & temp(BIN_IN_LENGTH-1);
 
       -- shift temp left by 1 bit
@@ -59,8 +61,12 @@ begin
     
     end loop;
  
-    -- set output after type conversion
-    bcd_out <= std_logic_vector(bcd_temp);
+    -- set output after type conversion.
+    -- again, the complicated looking formula for indexing the bcd_temp variable
+    -- is to obtain the individual BCD digits and place them into the bcd_out array
+    for i in 0 to (NUM_BCD_DIGITS-1) loop
+        bcd_out(i) <= std_logic_vector(bcd_temp((3+4*i) downto i*4));
+    end loop;
 
   end process;          
   
--- testbenches/tb_bin2bcd.vhd
+++ testbenches/tb_bin2bcd.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.digit_types.bcd_digit;
  
 entity tb_bin2bcd is
 end tb_bin2bcd;
@@ -12,22 +13,23 @@ architecture behavior of tb_bin2bcd is
         );
         port ( 
             bin_in : in  std_logic_vector ((BIN_IN_LENGTH-1) downto 0);
-            bcd_out: out std_logic_vector((NUM_BCD_DIGITS*4-1) downto 0)
+            bcd_out: out bcd_digit((NUM_BCD_DIGITS-1) downto 0)
         );      
     end component;
 
     constant BIN_IN_LENGTH : integer := 16;
     constant NUM_BCD_DIGITS: integer := 4;  
 
-    --inputs
     signal bin_in : std_logic_vector((BIN_IN_LENGTH-1) downto 0) := (others => '0');
-    signal clk : std_logic := '0';  -- can be omitted
 
-    --outputs
-    signal bcd_out : std_logic_vector((NUM_BCD_DIGITS*4-1) downto 0);
+    -- (can be omitted)
+    signal clk : std_logic := '0';
+
+    signal bcd_out : bcd_digit((NUM_BCD_DIGITS-1) downto 0) := (others => (others => '0'));
 
     -- clock period definitions
-    constant CLK_PERIOD : time := 10 ns;  -- can be omitted
+    -- (can be omitted)
+    constant CLK_PERIOD : time := 10 ns;
 
 begin
  
@@ -63,23 +65,28 @@ begin
 
       -- should return 4095
       bin_in <= x"0fff";
-      wait for CLK_PERIOD*10;  assert bcd_out = x"4095" severity error; 
+      wait for CLK_PERIOD*10;  
+      assert bcd_out(3) & bcd_out(2) & bcd_out(1) & bcd_out(0) = x"4095" severity error; 
 
       -- should return 0
       bin_in <= x"0000";
-      wait for CLK_PERIOD*10;  assert bcd_out = x"0000" severity error;
+      wait for CLK_PERIOD*10;  
+      assert bcd_out(3) & bcd_out(2) & bcd_out(1) & bcd_out(0) = x"0000" severity error;
 
       -- should return 2748
       bin_in <= x"0abc";
-      wait for CLK_PERIOD*10;  assert bcd_out = x"2748" severity error;
+      wait for CLK_PERIOD*10;  
+      assert bcd_out(3) & bcd_out(2) & bcd_out(1) & bcd_out(0) = x"2748" severity error;
 
       -- should return 9999
       bin_in <= x"270F";
-      wait for CLK_PERIOD*10;  assert bcd_out = x"9999" severity error;
+      wait for CLK_PERIOD*10; 
+      assert bcd_out(3) & bcd_out(2) & bcd_out(1) & bcd_out(0) = x"9999" severity error;
 
       -- should return 312
       bin_in <= x"0138";
-      wait for CLK_PERIOD*10;  assert bcd_out = x"0312" severity error;
+      wait for CLK_PERIOD*10;  
+      assert bcd_out(3) & bcd_out(2) & bcd_out(1) & bcd_out(0) = x"0312" severity error;
 		
       wait;
    end process;
