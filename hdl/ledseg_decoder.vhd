dddc28b1792e2ca1eba886e004f4900ad1028a8b
sir-drako <67859416+sir-drako@users.noreply.github.com>
2021-03-27 07:41:44 +0100

    Fix ledseg assignments to match Cyclone V dev kit (MSB is G seg)

--- hdl/ledseg_decoder.vhd
+++ hdl/ledseg_decoder.vhd
@@ -1,8 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- COMMON ANODE LED 7 SEGMENT DECODER (one digit)
--- Segments: A B C D E F G
+-- COMMON ANODE LED 7 SEGMENT DECODER (n digits)
+-- Segments: G F E D C B A
 
 entity ledseg_decoder is 
     port (
@@ -17,16 +17,16 @@ begin
     process (decimal_in)
     begin
         case (decimal_in) is 
-            when x"0"  => ledseg_out <= b"0000001";
-            when x"1" => ledseg_out <= b"1001111";
-            when x"2" => ledseg_out <= b"0010010";
-            when x"3" => ledseg_out <= b"0000110";
-            when x"4" => ledseg_out <= b"1001100";
-            when x"5" => ledseg_out <= b"0100100";
-            when x"6" => ledseg_out <= b"0100000";
-            when x"7" => ledseg_out <= b"0001111";
+            when x"0"  => ledseg_out <= b"1000000";
+            when x"1" => ledseg_out <= b"1111001";
+            when x"2" => ledseg_out <= b"0100100";
+            when x"3" => ledseg_out <= b"0110000";
+            when x"4" => ledseg_out <= b"0011001";
+            when x"5" => ledseg_out <= b"0010010";
+            when x"6" => ledseg_out <= b"0000010";
+            when x"7" => ledseg_out <= b"1011000";
             when x"8" => ledseg_out <= b"0000000";
-            when x"9" => ledseg_out <= b"0000100";
+            when x"9" => ledseg_out <= b"0010000";
 
             -- invalid inputs turn off all segments
             when others => ledseg_out <= b"1111111";
--- hdl/n_ledseg_decoder.vhd
+++ hdl/n_ledseg_decoder.vhd
@@ -4,14 +4,14 @@ use work.digit_types.ledseg_digit;
 use work.digit_types.bcd_digit;
 
 -- COMMON ANODE LED 7 SEGMENT DECODER (n digits)
--- Segments: A B C D E F G
+-- Segments: G F E D C B A
 entity n_ledseg_decoder is 
     generic (
         NUM_DIGITS : integer := 4
     );
     port (
         decimal_in : in bcd_digit((NUM_DIGITS-1) downto 0);
-        ledseg_out : out ledseg_digit(NUM_DIGITS-1 downto 0) := (others => (others => '0'))
+        ledseg_out : out ledseg_digit(NUM_DIGITS-1 downto 0) := (others => (others => '1'))
     );
 end entity;
 
